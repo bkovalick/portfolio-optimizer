c03db1907dd2485f18106f8968c365ff864332ac
Benjamin Kovalick <kovalickbenjamin@gmail.com>
2026-06-08 20:39:19 -0400

    Perform refactor myself

--- src/reporting/signal_monitoring.py
+++ src/reporting/signal_monitoring.py
@@ -4,16 +4,18 @@ import numpy as np
 from scipy.stats import spearmanr
 from scipy import stats
 from models.monitoring_stats import MonitoringStats
-
+        
 class BaseSignalMonitor(abc.ABC):
 
-    @property
     @abc.abstractmethod
-    def _scores(self) -> pd.DataFrame: ...
+    @property
+    def forward_data(self):
+        ...
 
-    @property
     @abc.abstractmethod
-    def _forward_data(self) -> pd.DataFrame: ...
+    @property
+    def scores(self):
+        ...
 
     def analyze(self) -> MonitoringStats:
         ic_series = self._compute_ic_statistics()
@@ -23,27 +25,35 @@ class BaseSignalMonitor(abc.ABC):
         )
 
     def _compute_ic_statistics(self) -> pd.Series:
+        """
+        Computes the Spearman rank correlation (IC) between the signal and 
+        forward returns for each date, then applies a rolling mean to smooth the series.
+        """
         ic_values = []
-        for date in self._scores.index:
-            if date not in self._forward_data.index:
+        for date in self.scores.index:
+            if date not in self.forward_data.index:
                 continue
 
-            scores = self._scores.loc[date].dropna()
-            fwd = self._forward_data.loc[date].dropna()
-            common = scores.index.intersection(fwd.index)
+            scores = self.scores.loc[date].dropna()
+            fwd_data = self.forward_data.loc[date].dropna()
+            common = scores.index.intersection(fwd_data.index)
             if len(common) < 5:
                 continue
 
-            ic, _ = spearmanr(scores.loc[common], fwd.loc[common])
+            ic, _ = spearmanr(scores.loc[common], fwd_data.loc[common])
             ic_values.append((date, ic))
-
+    
         if not ic_values:
             return pd.Series(dtype=float)
-
+        
         dates, ics = zip(*ic_values)
         return pd.Series(ics, index=dates)
 
     def _compute_ic_summary(self, ic_series: pd.Series) -> dict:
+        """
+        Perform a one-sample t-test to determine if the mean IC is significantly different from zero.
+        Returns the t-statistic and p-value.
+        """
         if len(ic_series.dropna()) < 2:
             return {"t_statistic": np.nan, "p_value": np.nan}
         t_stat, p_value = stats.ttest_1samp(ic_series.dropna(), 0)
@@ -53,44 +63,52 @@ class BaseSignalMonitor(abc.ABC):
             "mean_ic": ic_series.mean(),
             "ic_ir": ic_ir,
             "hit_rate": float((ic_series > 0).mean()),
-            "t_statistic": t_stat,
+            "t_statistic": t_stat, 
             "p_value": p_value,
             "half_life": self._compute_half_life(ic_series),
             "n_observations": len(ic_series.dropna())
         }
-
+    
     def _compute_half_life(self, ic_series: pd.Series) -> float:
+        """
+        Estimate the signal decay half-life from signals using AR(1) autocorrelation.
+
+        Fits a first-order autoregressive model and solves for the number of periods
+        it takes for the autocorrelation to decay to half its initial value.
+        Returns np.nan when phi is outside (0, 1), i.e. the series is non-stationary,
+        mean-reverting with no persistence, or negatively autocorrelated.
+        """
         phi = ic_series.autocorr(lag=1)
+
         if phi <= 0 or phi >= 1:
             return np.nan
-        return np.log(0.5) / np.log(phi)
 
+        half_life = np.log(0.5) / np.log(phi)
+        return half_life            
+
+class PairsICDiagnostics(BaseSignalMonitor):
+    def __init__(self, 
+                 pairs_cache: pd.DataFrame):
+        self.pairs_cache = pairs_cache
+        self.forward_spread = pd.DataFrame()
+        self.zscores = pd.DataFrame()
+    
+    def forward_data(self):
+        return self.forward_spread
+    
+    def scores(self):
+        return -self.zscores
+    
 class LongOnlyICDiagnostics(BaseSignalMonitor):
-    def __init__(self,
+    """Monitors signal decay by computing rolling Information Coefficient and half-life of those signals."""
+    def __init__(self, 
                  forward_returns: pd.DataFrame,
                  signal: pd.DataFrame):
         self.forward_returns = forward_returns
         self.signal = signal
 
-    @property
-    def _scores(self) -> pd.DataFrame:
-        return self.signal
-
-    @property
-    def _forward_data(self) -> pd.DataFrame:
+    def forward_data(self):
         return self.forward_returns
-
-class PairsICDiagnostics(BaseSignalMonitor):
-    def __init__(self,
-                 pairs_cache: pd.DataFrame):
-        self.pairs_cache = pairs_cache
-        self.forward_spread = pd.DataFrame()
-        self.zscores = pd.DataFrame()
-
-    @property
-    def _scores(self) -> pd.DataFrame:
-        return -self.zscores
-
-    @property
-    def _forward_data(self) -> pd.DataFrame:
-        return self.forward_spread
+    
+    def scores(self):
+        return self.signal
